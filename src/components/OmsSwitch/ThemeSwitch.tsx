35a3e85b2afd38ee924f73ff34e239c40cb11a49
lixin59 <14532887107@qq.com>
2021-10-30 15:44:36 +0800

    [FIX]: 修复使用ThemeSwitch 组件没有传递回调函数而不能选择开关的错误

--- src/components/OmsSwitch/ThemeSwitch.tsx
+++ src/components/OmsSwitch/ThemeSwitch.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import styles from './style';
 
@@ -9,18 +9,20 @@ type tProps = {
 
 export default function ThemeSwitch({ checked = false, onChange, ...props }: tProps) {
   const classes = makeStyles(styles)();
+  const [isChecked, setIsChecked] = useState(checked);
 
   const changeChecked = () => {
     if (onChange) {
       onChange();
     }
+    setIsChecked(!isChecked);
   };
 
   return (
     <div className={classes.switchBox}>
       <div className={classes.switchIconMoon}></div>
       <div className={classes.switchIconSun}></div>
-      <div className={checked ? classes.switchChecked : classes.switchNoChecked} onClick={changeChecked}></div>
+      <div className={isChecked ? classes.switchChecked : classes.switchNoChecked} onClick={changeChecked}></div>
     </div>
   );
 }
