9f96eef317e43ce4d2f8136fae90ea02fc4766de
lixin59 <1453287107@qq.com>
2022-06-18 16:21:15 +0800

    ref(them): 将主题相关的内容迁移到theming

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,9 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import OmsSnackbar from './components/Snackbars';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ThemeSwitch from './components/OmsSwitch/ThemeSwitchInput';
-import { ThemeProvider, createTheme, makeStyles, createStyles } from '@material-ui/core/styles'; // 4.x.x 版本
+import { ThemeProvider, makeStyles, createStyles } from '@material-ui/core/styles'; // 4.x.x 版本
 // import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'; // 5.x.x 版本
-
-declare module '@material-ui/core/styles/createPalette' {
-  interface Palette {
-    loading: Palette['primary'];
-    snacbar: Palette['primary'];
-    tag: Palette['primary'];
-    tab: Palette['primary'];
-    boxShadowInset: Palette['primary'];
-  }
-  interface PaletteOptions {
-    loading: PaletteOptions['primary'];
-    snacbar: PaletteOptions['primary'];
-    tag: PaletteOptions['primary'];
-    tab: PaletteOptions['primary'];
-    boxShadowInset: PaletteOptions['primary'];
-  }
-}
+import { themeLight, themeDark } from './theming';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -48,78 +32,6 @@ const useStyles = makeStyles(() =>
     }
   })
 );
-
-const themeLight = createTheme({
-  palette: {
-    background: {
-      paper: '#fdfdfd'
-    },
-    boxShadowInset: {
-      main: '#ccc'
-    },
-    type: 'light',
-    grey: {
-      A100: '#f5f5f5', // BodyBox
-      A200: '#d5d5d5', // Navigation
-      A700: '#ffffff' // omsSelect
-    },
-    loading: {
-      main: '#0ba8a8'
-    },
-    primary: {
-      main: '#4caf50'
-    },
-    snacbar: {
-      light: '#b5f3b5',
-      dark: 'rgba(181, 243, 181, 0.1)',
-      main: 'rgba(172, 241, 241, 0.25)'
-      // main: '#acf1f1'
-    },
-    tab: {
-      main: '#F5F5F5'
-    },
-    tag: {
-      light: '#99CC99',
-      main: '#66CCCC'
-    }
-  }
-});
-
-const themeDark = createTheme({
-  palette: {
-    boxShadowInset: {
-      main: '#040404'
-    },
-    type: 'dark',
-    grey: {
-      A100: '#2b2b2b',
-      A200: '#3c3f41',
-      A700: '#31343d' // omsSelect
-    },
-    loading: {
-      main: '#74ffff'
-    },
-    primary: {
-      main: '#5fb878'
-    },
-    snacbar: {
-      light: '#659d4d',
-      dark: 'rgba(101,157,77,0.25)',
-      main: 'rgba(0,153,153,0.25)'
-      // main: '#009999',
-    },
-    tab: {
-      main: '#2a2a2a'
-    },
-    tag: {
-      light: '#74b95c',
-      main: '#009999'
-    },
-    text: {
-      primary: '#ffffff'
-    }
-  }
-});
 
 const OmsRouter = () => {
   return <>{useRoutes(router)}</>;
---
+++ src/theming/index.ts
@@ -0,0 +1,120 @@
+import { createTheme } from '@material-ui/core/styles'; // 5.x.x 版本
+
+// declare module '@mui/material/styles' {
+//   interface Theme {
+//     status: {
+//       danger: string;
+//     };
+//   }
+//   // 允许配置文件使用 `createTheme`
+//   interface ThemeOptions {
+//     status?: {
+//       danger?: string;
+//     };
+//   }
+//   interface Palette {
+//     loading: Palette['primary'];
+//     snacbar: Palette['primary'];
+//     tag: Palette['primary'];
+//     tab: Palette['primary'];
+//     type: 'light' | 'dark';
+//     boxShadowInset: Palette['primary'];
+//   }
+//   interface PaletteOptions {
+//     loading: PaletteOptions['primary'];
+//     snacbar: PaletteOptions['primary'];
+//     tag: PaletteOptions['primary'];
+//     tab: PaletteOptions['primary'];
+//     type: 'light' | 'dark';
+//     boxShadowInset: PaletteOptions['primary'];
+//   }
+// }
+
+declare module '@material-ui/core/styles/createPalette' {
+  interface Palette {
+    loading: Palette['primary'];
+    snacbar: Palette['primary'];
+    tag: Palette['primary'];
+    tab: Palette['primary'];
+    boxShadowInset: Palette['primary'];
+  }
+  interface PaletteOptions {
+    loading: PaletteOptions['primary'];
+    snacbar: PaletteOptions['primary'];
+    tag: PaletteOptions['primary'];
+    tab: PaletteOptions['primary'];
+    boxShadowInset: PaletteOptions['primary'];
+  }
+}
+
+export const themeLight = createTheme({
+  palette: {
+    background: {
+      paper: '#fdfdfd'
+    },
+    boxShadowInset: {
+      main: '#ccc'
+    },
+    type: 'light',
+    grey: {
+      A100: '#f5f5f5', // BodyBox
+      A200: '#d5d5d5', // Navigation
+      A700: '#ffffff' // omsSelect
+    },
+    loading: {
+      main: '#0ba8a8'
+    },
+    primary: {
+      main: '#4caf50'
+    },
+    snacbar: {
+      light: '#b5f3b5',
+      dark: 'rgba(181, 243, 181, 0.1)',
+      main: 'rgba(172, 241, 241, 0.25)'
+      // main: '#acf1f1'
+    },
+    tab: {
+      main: '#F5F5F5'
+    },
+    tag: {
+      light: '#99CC99',
+      main: '#66CCCC'
+    }
+  }
+});
+
+export const themeDark = createTheme({
+  palette: {
+    boxShadowInset: {
+      main: '#040404'
+    },
+    type: 'dark',
+    grey: {
+      A100: '#2b2b2b',
+      A200: '#3c3f41',
+      A700: '#31343d' // omsSelect
+    },
+    loading: {
+      main: '#74ffff'
+    },
+    primary: {
+      main: '#5fb878'
+    },
+    snacbar: {
+      light: '#659d4d',
+      dark: 'rgba(101,157,77,0.25)',
+      main: 'rgba(0,153,153,0.25)'
+      // main: '#009999',
+    },
+    tab: {
+      main: '#2a2a2a'
+    },
+    tag: {
+      light: '#74b95c',
+      main: '#009999'
+    },
+    text: {
+      primary: '#ffffff'
+    }
+  }
+});
