35af00a6464a61125212ade1df9e68863d000124
lixin <1453287107@qq.com>
2022-06-20 18:02:59 +0800

    chore(vite.config): 修改配置，构建目标浏览器的版本

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: ['chrome87', 'edge88', 'es2021', 'firefox78', 'safari14']
+  },
+  plugins: [react()]
 });
