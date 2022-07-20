4bf6964b647f32075b0dc845f21d6433a6bbd3d3
lixin59 <1453287107@qq.com>
2022-07-20 20:09:08 +0800

    chore(redux-logger): dev环境添加log信息

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,9 +3,23 @@ import reducer from './reducer';
 import createSagaMiddleware from 'redux-saga';
 // import {rootSaga} from './saga' //其实是一个generator
 import { defSaga } from '../sagas/index';
+import { createLogger } from 'redux-logger';
 
+const middlewares: any[] = [];
 const sagaMiddleware = createSagaMiddleware();
-const store = applyMiddleware(sagaMiddleware)(createStore)(reducer);
+middlewares.push(sagaMiddleware);
+
+if (import.meta.env.DEV) {
+  const logger = createLogger({
+    level: 'info',
+    collapsed: true,
+    duration: true,
+    diff: true
+  });
+  middlewares.push(logger);
+}
+
+const store = applyMiddleware(...middlewares)(createStore)(reducer);
 // 负责执行saga
 sagaMiddleware.run(defSaga);
 export default store;
