14a14492dbaa8fd07ed345305498815f887ad4e8
lixin59 <1453287107@qq.com>
2022-07-30 16:38:06 +0800

    perf(fileBrowser): 优化用户体验
    1.文件预览实现一键复制功能，文本超出自动换行展示

--- src/components/OmsSyntaxHighligh/index.tsx
+++ src/components/OmsSyntaxHighligh/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus, coyWithoutShadows, darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useSnackbar } from 'notistack';
+import IconButton from '@material-ui/core/IconButton';
+import CopyAllIcon from '@mui/icons-material/CopyAll';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import Tooltip from '@material-ui/core/Tooltip';
 
 type tProps = {
   textContent: string;
   language: string;
   darkMode?: boolean;
-}
+};
 
 const them = {
   dark: vscDarkPlus,
@@ -21,16 +26,56 @@ const OmsSyntaxHighlight = (props: tProps) => {
   if (typeof darkMode === 'boolean') {
     them.light = coyWithoutShadows;
   }
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const copy = (content: string) => {
+    navigator.clipboard
+      .writeText(content)
+      .then(
+        function () {
+          /* clipboard successfully set */
+          enqueueSnackbar('复制成功', {
+            autoHideDuration: 3000,
+            variant: 'success'
+          });
+        },
+        function () {
+          /* clipboard write failed */
+          enqueueSnackbar(`复制失败`, {
+            autoHideDuration: 3000,
+            variant: 'error'
+          });
+        }
+      )
+      .catch((e) => {
+        enqueueSnackbar(`复制失败；${JSON.stringify(e)}`, {
+          autoHideDuration: 3000,
+          variant: 'error'
+        });
+      });
+  };
+
   return (
-    <SyntaxHighlighter
-      showLineNumbers={true}
-      lineNumberStyle={{ color: '#ddd', fontSize: 10 }}
-      style={darkMode ? them.dark : them.light}
-      language={language}
-      PreTag='div'
-    >
-      {String(textContent).replace(/\n$/, '')}
-    </SyntaxHighlighter>
+    <>
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+        <Tooltip title="复制内容" placement="left-start">
+          <IconButton aria-label="hostCard" color="secondary" size="small" onClick={() => copy(textContent)}>
+            <CopyAllIcon />
+          </IconButton>
+        </Tooltip>
+      </div>
+      <SyntaxHighlighter
+        lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
+        showLineNumbers={true}
+        lineNumberStyle={{ color: '#ddd', fontSize: 10 }}
+        style={darkMode ? them.dark : them.light}
+        language={language}
+        wrapLines={true}
+        PreTag="div">
+        {String(textContent).replace(/\n$/, '')}
+      </SyntaxHighlighter>
+    </>
   );
 };
 
