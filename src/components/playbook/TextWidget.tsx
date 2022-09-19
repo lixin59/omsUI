25a465b2be779a59915ed1650f7a1bbbde8fa7f5
lixin <1453287107@qq.com>
2022-09-19 12:07:32 +0800

    fix(playbook): 修复TextWidget组件没有获取默认值的问题

--- src/components/playbook/TextWidget.tsx
+++ src/components/playbook/TextWidget.tsx
@@ -2,18 +2,36 @@ import * as React from 'react';
 import TextField from '../OmsTextField/index';
 import { WidgetProps } from '@rjsf/core';
 
+function getValue(value: any, defaultValue: any): any {
+  if (typeof value === 'undefined' && typeof defaultValue === 'undefined') {
+    return '';
+  }
+  if (typeof value !== 'undefined' && typeof defaultValue === 'undefined') {
+    return value;
+  }
+  if (typeof value === 'undefined' && typeof defaultValue !== 'undefined') {
+    return defaultValue;
+  }
+  if (typeof value !== 'undefined' && typeof defaultValue !== 'undefined') {
+    return value;
+  }
+  return value;
+}
+
 export default function TextWidget(props: WidgetProps) {
-  const { id, options, label, placeholder, value, required, disabled, autofocus, onChange, onBlur, onFocus } = props;
+  const { id, options, label, placeholder, value, required, schema, disabled, autofocus, onChange, onBlur, onFocus } =
+    props;
   const _onChange = ({ target: { value } }) => {
-    return onChange(value === '' ? options.emptyValue : value);
+    return onChange(value === '' ? (options.emptyValue ? options.emptyValue : value) : value);
   };
+
   return (
     <TextField
       style={{ width: '100%' }}
       id={id}
       label={label}
       variant="outlined"
-      value={value ? value : ''}
+      value={getValue(value, schema.default)}
       placeholder={placeholder}
       required={required}
       disabled={disabled}
