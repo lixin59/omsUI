f921bfa04f38c3bb09783fa4c68d169606f82d82
lixin <1453287107@qq.com>
2022-12-19 16:05:08 +0800

    chore(package): 升级构建依赖，移除无用的依赖项

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'google',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3002
+  },
   build: {
     target: ['chrome87', 'edge88', 'es2021', 'firefox78', 'safari14']
   },
